bbe4ea05ddbf1024df78788f539e5605e3d13859
fzed51 <fzed51+git@gmail.com>
2016-10-14 16:15:20 +0200

    Mise en place des test

--- kahlan.ps1
+++ kahlan.ps1
@@ -1,1 +1,13 @@
-phpdbg.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --coverage --reporter=bar
+$oldcomspec = $env:ComSpec
+$oldEncoding = $OutputEncoding
+$env:ComSpec = $null;
+[System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$env:TERM = 'ANSI'
+
+phpdbg.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --coverage --reporter=verbose
+ 
+pause
+
+$env:ComSpec = $oldcomspec
+$env:TERM = $null
+[System.Console]::OutputEncoding = $oldEncoding
