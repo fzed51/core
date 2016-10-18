4a13b0335057fa08b95c5be96fbcc66144a67e04
fzed51 <fzed51+git@gmail.com>
2016-10-18 10:57:03 +0200

    Modification du script de test

--- kahlan.ps1
+++ kahlan.ps1
@@ -1,10 +1,18 @@
+Param(
+    [switch]$coverage
+)
+
 $oldcomspec = $env:ComSpec
 $oldEncoding = $OutputEncoding
 $env:ComSpec = $null;
 [System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $env:TERM = 'ANSI'
 
-phpdbg.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --coverage --reporter=verbose
+if($coverage){
+    phpdbg.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --coverage --reporter=verbose
+}else{
+    php.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --reporter=verbose
+}
  
 pause
 
