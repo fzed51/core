8160cb17d18cf1533e66b2d09f8d117803d0657e
fzed51 <fzed51+git@gmail.com>
2016-10-19 12:02:24 +0200

    séparation de la méthode d'appel pour les test

--- spec/route.Spec.php
+++ spec/route.Spec.php
@@ -42,6 +42,14 @@ describe('Route test', function() {
             ]);
             expect($new_route->pathToRegEx())->toBe('`^uri/(?<id>[0-9]+)$`');
         });
+        it('should interpret a complex path with complex parameter', function() {
+            $new_route = new Route('name_path', 'uri/{id}/action/{hexa}', 'action@controleur');
+            $new_route->setRules([
+                "id" => "[0-9]+",
+                "hexa" => "[0-9a-fA-F]{2}"
+            ]);
+            expect($new_route->pathToRegEx())->toBe('`^uri/(?<id>[0-9]+)/action/(?<hexa>[0-9a-fA-F]{2})$`');
+        });
         it('should interpret a simple path with complex parameter with capturing group', function() {
             $new_route = new Route('name_path', 'uri/{slug}', 'action@controleur');
             $new_route->setRules([
@@ -54,21 +62,13 @@ describe('Route test', function() {
     describe('Execution', function() {
 
         it('should execut an route with action/controler action', function() {
-            
-            class foo{ public function bar(){} public function bad(){} }
-
+            class foo{ public function bar(){expect(true)->toBe(true);} public function bad(){expect(true)->toBe(false);} }
             $new_route = new Route('name_path', 'uri', 'bar@foo');
-            expect('call_user_func')->toBeCalled()->with(Arg::toBeA('array'));
-            expect('call_user_func')->toBeCalled()->with(Arg::toContain('bar'));
             $new_route->executeAction();
         });
-
         it('should execut an route with callback action', function() {
-
-            $callback = function(){};
-            
+            $callback = function(){expect(true)->toBe(true);};
             $new_route = new Route('name_path', 'uri', $callback);
-            expect('call_user_func')->toBeCalled()->with(Arg::toBe($callback));
             $new_route->executeAction();
         });
         
