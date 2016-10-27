4255e53ded7ca826ad931f7ac2d55a8891be3ce1
fzed51 <fzed51+git@gmail.com>
2016-10-27 10:11:07 +0200

    amelioration du script de test

--- kahlan.ps1
+++ kahlan.ps1
@@ -1,10 +1,19 @@
+Param(
+    [switch]$coverage,
+    [string]$reporter = "verbose"
+)
+
 $oldcomspec = $env:ComSpec
 $oldEncoding = $OutputEncoding
 $env:ComSpec = $null;
 [System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $env:TERM = 'ANSI'
 
-phpdbg.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --coverage --reporter=verbose
+if($coverage){
+    phpdbg.exe -qrr .\vendor\kahlan\kahlan\bin\kahlan --coverage --reporter=$reporter
+}else{
+    php.exe .\vendor\kahlan\kahlan\bin\kahlan --reporter=$reporter
+}
  
 pause
 
